a3e21fd24935c7e75da64a900869007594cdc73a
Benjamin Dummer <dummerbenjamin@gmail.com>
2015-09-01 08:22:28 -0400

    Fixed setup.py, version bump, new release

--- setup.py
+++ setup.py
@@ -1,25 +1,46 @@
 import os
+
 from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+VERSION = '0.0.4'
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
 setup(
     name='django-composer',
 
-    version='0.0.2',
+    version=VERSION,
 
-    packages=['composer'],
-    package_data={
-        'composer': ['__init__.py', 'models.py', 'urls.py', 'views.py'],
-        'composer/templates/composer': ['example.html', 'edit.html'],
-        'composer/static/js': ['composer.js', 'composer.min.js'],
-        'composer/static/css': ['composer.css'],
-        'composer/migrations': ['0001_initial.py', '__init__.py'],
-        'composer/templatetags': ['composer.py', '__init__.py']
-    },
+    packages=get_packages('composer'),
+    package_data=get_package_data('composer'),
 
     license='MIT License',
 
@@ -29,6 +50,7 @@ setup(
     keywords='',
 
     url='https://github.com/featherweightweb/django-composer',
+    download_url='https://github.com/featherweightweb/django-composer/tarball/%s' % VERSION,
 
     author='Benjamin Dummer',
     author_email='ben@featherweightweb.com',
