2db7bdc2baf602e330d7b6950e3b5a047a3523b0
Benjamin Dummer <dummerbenjamin@gmail.com>
2015-09-01 09:23:10 -0400

    Added admin config

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
